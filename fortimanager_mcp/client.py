566c39282f8223493631297d988e7c2e9d9f83ac
Alan Wodarski <alan.k.wodarski@xcelenergy.com>
2026-08-06 12:46:59 -0500

    fixed timeout

--- fortimanager_mcp/client.py
+++ fortimanager_mcp/client.py
@@ -89,7 +89,7 @@ class FortiManagerClient:
 
         self._http = httpx.Client(
             verify=verify_ssl,
-            timeout=httpx.Timeout(connect=10.0, read=120.0, write=10.0, pool=5.0),
+            timeout=httpx.Timeout(connect=10.0, read=300.0, write=10.0, pool=5.0),
             headers={"Content-Type": "application/json"},
         )
 
